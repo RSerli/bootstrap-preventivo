58ee548e3c65144a014a85bb6b07c4595a07aae8
Rserli <riccardoserli.95@gmail.com>
2025-05-28 13:10:26 +0200

    Js Elements from Dom + variables

--- script/app.js
+++ script/app.js
@@ -1,1 +1,15 @@
 // console.log("TEST")
+
+//  Getting elements from DOM
+const FormElement = document.getElementById("QuotationForm")
+console.dir(FormElement)
+const SelectedQuotationWork = FormElement.querySelector('#SelectQuotationWork')
+console.dir(SelectedQuotationWork)
+const UserPromo = FormElement.querySelector('#inputPromo')
+console.dir(UserPromo)
+
+// Utility Variables
+
+const PRICE_BACKEND = 33.5 // number
+const PRICE_FRONTEND = 33.5 // number
+const PRICE_BPROJECT = 33.5 // number
