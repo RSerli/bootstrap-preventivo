4af25a7d0766058b5de44552cfe9373cb5b840f1
Rserli <riccardoserli.95@gmail.com>
2025-05-28 20:49:30 +0200

    Setted all Variables

--- script/app.js
+++ script/app.js
@@ -1,6 +1,8 @@
 // console.log("TEST")
 
-//  Getting elements from DOM
+/*
+*   --- Getting elements from DOM ---
+*/
 const FormElement = document.getElementById("QuotationForm")
 console.dir(FormElement)
 const SelectedQuotationWork = FormElement.querySelector('#SelectQuotationWork')
@@ -8,8 +10,23 @@ console.dir(SelectedQuotationWork)
 const UserPromo = FormElement.querySelector('#inputPromo')
 console.dir(UserPromo)
 
-// Utility Variables
+/*
+*   --- Utility Variables ---
+*/
+const PRICE_BACKEND = 20.50 // number float
+const PRICE_FRONTEND = 15.30 // number float
+const PRICE_BPROJECT = 33.60 // number float
 
-const PRICE_BACKEND = 33.5 // number
-const PRICE_FRONTEND = 33.5 // number
-const PRICE_BPROJECT = 33.5 // number
+const ValidPromoCodes = [
+    "YHDNU32",
+    "JANJC63",
+    "PWKCN25",
+    "SJDPO96",
+    "POCIE24"
+]
+
+const DiscountPromoCode = 25 // number integer
+
+/*
+*   --- Utility Functions ---
+*/
